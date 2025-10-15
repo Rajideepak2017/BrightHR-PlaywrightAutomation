f72c9315f2df64850a85fcf121a8053da8cb8493
Rajideepak2017 <rajalakshmi.deepak2017@gmail.com>
2025-10-15 15:57:22 +0100

    Update employeePage.js

--- pageObjects/employeePage.js
+++ pageObjects/employeePage.js
@@ -114,7 +114,7 @@ async addEmployee( firstName, lastName, email,day,month,year ) {
 }
 
 
-  async getEmployeeNamesstash() {
+  async getEmployeeNames() {
 
     const nameLocator = this.page.locator(this.employeeLists);
     const count = await nameLocator.count();
