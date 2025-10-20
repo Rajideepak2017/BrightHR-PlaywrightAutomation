5ca25766b81d557080140903018cf6eb0783bd5b
badrideepak@gmail.com <badrideepak@gmail.com>
2025-10-20 14:06:28 +0100

    removed 88

--- pageObjects/employeePage.js
+++ pageObjects/employeePage.js
@@ -4,7 +4,7 @@ class employeePage{
         this.page = page;
         this.employeeMenu = "//*[@title='Employees']";
         this.addEmployeelink = "//*[text()='Add employee']";
-        this.firstName='#firstName';
+        this.firstName='#firstName88';
         this.lastName='#lastName';
         this.email ='#email';
         this.submit="//*[@type='submit']";
